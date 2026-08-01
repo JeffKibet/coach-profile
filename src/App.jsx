ec96e24f0cd70c53783436e36c9582d4515d04fa
jeff <jefferykibet@gmail.com>
2026-08-01 11:19:53 +0300

    feat: refactor CoachProfile component, remove unused CSS file, and clean up imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-import CoachProfile from "./components/CoachProfile";
+import './App.css';
+import CoachProfile from './component/CoachProfile';
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <CoachProfile />
+      <CoachProfile/>
     </>
   )
 }
--- src/component/CoachProfile.jsx
+++ src/component/CoachProfile.jsx
@@ -1,3 +1,13 @@
+//import "./Profile.css";
+
+//const TrainingList = ["Point guards", "Small fowards", "Team strategies"];
 
 
-export default CoachProfile ;
+function CoachProfile () {
+    return (<div ClassName="profile">
+    <h1 ClassName="label">Ken Wachira</h1>;
+    </div>
+    )
+}
+
+export default CoachProfile;
