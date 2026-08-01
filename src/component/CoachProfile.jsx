d436634d2ffdc41b5905b2066a34a87e5a2494e6
jeff <jefferykibet@gmail.com>
2026-08-01 13:11:14 +0300

    feat: restructure CoachProfile component layout, update styles in Profile.css, and remove center alignment in index.css

--- src/component/CoachProfile.jsx
+++ src/component/CoachProfile.jsx
@@ -4,20 +4,28 @@ import "./Profile.css";
 
 
 function CoachProfile () {
-    return (<div className="profile">
-    <h1 className="label">Ken Wachira</h1>
+    return (
+      <div className="page">
+        <div className="profile">
+          <h1 className="label">Ken Wachira</h1>
 
-    <div className="Title">
-        <h3>BasketBall Coach</h3>
+          <div className="Title">
+            <h3>BasketBall Coach</h3>
 
-        <div className="About">
-            <h5>About</h5>
-            <p>Meet Ken Wachira, a Basketball strategist known for his quick decision-making and game-reading abilities. His coaching style emphasizes enhancing court vision and predicting oponents moves. Gain exclusive insights into basketball tactics and honing your 3-point game with Ken Wachira, the maestro behind the arc</p>
-
+            <div className="About">
+              <h5>About</h5>
+              <p>
+                Meet Ken Wachira, a Basketball strategist known for his quick
+                decision-making and game-reading abilities. His coaching style
+                emphasizes enhancing court vision and predicting oponents moves.
+                Gain exclusive insights into basketball tactics and honing your
+                3-point game with Ken Wachira, the maestro behind the arc
+              </p>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
-    )
+      </div>
+    );
 }
 
 export default CoachProfile;
