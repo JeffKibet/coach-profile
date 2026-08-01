0970b68992cfb6b001423b0d1626fc4f18a2a347
jeff <jefferykibet@gmail.com>
2026-08-01 10:26:28 +0300

    feat: add SVG assets for React and Vite, create initial CoachProfile component and styles, set up global CSS variables, and configure Vite for React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import CoachProfile from "./components/CoachProfile";
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <CoachProfile />
+    </>
+  )
+}
+
+export default App
