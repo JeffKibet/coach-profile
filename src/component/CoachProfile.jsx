56d920c1ee1795b105dd4e95535620abed8e7bdd
jeff <jefferykibet@gmail.com>
2026-08-01 16:23:45 +0300

    feat: add footer text to CoachProfile component and update Profile.css

--- src/component/CoachProfile.jsx
+++ src/component/CoachProfile.jsx
@@ -7,33 +7,34 @@ function CoachProfile () {
     return (
       <div className="page">
         <div className="left">
-            <img src="./assets/hero.png" alt="hero" className="hero-image"/>
+          <img src="./assets/hero.png" alt="hero" className="hero-image" />
 
-        <div className="profile">
-          <h1 className="name">Ken Wachira</h1><br></br>
+          <div className="profile">
+            <h1 className="name">Ken Wachira</h1>
+            <br></br>
 
-          <div className="Title">
-            <h3 className="role">BasketBall Coach</h3>
+            <div className="Title">
+              <h3 className="role">BasketBall Coach</h3>
 
-            <div className="About">
-              <h5>About</h5>
-              <p className="text">
-                Meet Ken Wachira, a Basketball strategist known for his quick
-                decision-making and game-reading abilities. His coaching style
-                emphasizes enhancing court vision and predicting oponents moves.
-                Gain exclusive insights into basketball tactics and honing your
-                3-point game with Ken Wachira, the maestro behind the arc
-              </p>
+              <div className="About">
+                <h5>About</h5>
+                <p className="text">
+                  Meet Ken Wachira, a Basketball strategist known for his quick
+                  decision-making and game-reading abilities. His coaching style
+                  emphasizes enhancing court vision and predicting oponents
+                  moves. Gain exclusive insights into basketball tactics and
+                  honing your 3-point game with Ken Wachira, the maestro behind
+                  the arc
+                </p>
+                <p className="footer">"Read the game, Own the court</p>
+              </div>
             </div>
-          </div>
-
-          <div className="right">
-            <div className="section">
-                <h2 className="label">Decisive Move Specialist</h2>
-            </div>
-
           </div>
         </div>
+        <div className="right">
+          <div className="section">
+            <h2 className="label">Decisive Move Specialist</h2>
+          </div>
         </div>
       </div>
     );
